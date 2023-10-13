a4413f2fd927c703b24bc6e28339502238fa0eba
Shahrukh <shahrukh1494@gmail.com>
2023-10-13 15:04:17 +0530

    custom components, dynamic values, reusing components, props, CSS styling and CSS modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import Post from "./components/Post";
+import Post from './components/Post';
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Shahrukh" body="React.js is awesome!" />
+      <Post author="Khan" body="React.js Summary!" />
+    </main>
+  );
 }
 
 export default App;
